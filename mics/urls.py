f258f7767f35e61e09fc0a8c5ea7d877df018fb8
dnyt <lyricaldnyt@gmail.com>
2018-06-14 10:45:27 +0300

    Created base.html

--- mics/urls.py
+++ mics/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns =[
-    #url(r'^image/(\d+)',views.image,name='viewImage'),
+    url(r'^image/(\d+)',views.image,name='viewImage'),
 ]
-#if settings.DEBUG:
-    #urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- mics/views.py
+++ mics/views.py
@@ -1,7 +1,7 @@
-#from django.shortcuts import render
-#from .models import Image
+from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
-#def image_list(request):
-#    images = Image.image_list()
-#    return render(request, 'index.html',{"images":images})
+def image_list(request):
+    images = Image.image_list()
+   return render(request, 'index.html',{"images":images})
