0d6a6813cc3e5a8397c1a15821b86457e1355caf
dnyt <lyricaldnyt@gmail.com>
2018-06-15 12:02:05 +0300

    Create view search functionality to query database

--- mics/views.py
+++ mics/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from .models import Image
 
 # Create your views here.
+
 def image_list(request):
     images = Image.image_list()
     return render(request, 'index.html',{"images":images})
+
+def search_results(request):
+
+    if 'image_category' in request.GET and request.GET["image_category"]:
+        searched_category = request.GET.get("image_category")
+        images = Image.search_by_category(searched_category)
+        messages = f"{searched_category}"
+
+        return render(request, 'search.html',{"message":message,"images":images,})
+        
