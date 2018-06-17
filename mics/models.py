7ae29f87bf4b6306e364a71a053a9dd2af587b46
dnyt <lyricaldnyt@gmail.com>
2018-06-17 16:59:57 +0300

    Create model Location test

--- mics/models.py
+++ mics/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    location = models.CharField(max_length =30)
+    location = models.CharField(max_length =30,blank = True)
 
     def __str__(self):
         return self.Location
 
     def save_location(self):
         self.save()
+
+    @classmethod
+    def update_location(cls,id,new_location):
+        cls.objects.filter(id).update(location = new_location)
+
+    @classmethod
+    def delete_location(cls,id):
+        cls.objects.filter(id).delete()
+
 
 class Category(models.Model):
     category = models.CharField(max_length =30,blank = True)
--- mics/tests.py
+++ mics/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Location,Image,Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.new_location= Location(location = 'Nairobi')
+        self.location.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        self.new_location.save_location()
+        location_id = self.new_location.id
+        Location.update_location(id, "mombasa")
+        self.assertEqual(self.new_location.location, "mombasa")
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def tearDown(self):
+        Location.objects.all().delete()
